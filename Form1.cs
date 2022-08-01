b20d234612385db3ed01d20aec5f0738c348e76d
siriusamro <73645885+siriusamro@users.noreply.github.com>
2022-08-01 14:24:17 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomTouckKeyboard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Process[] keys = Process.GetProcessesByName("TabTip");
+            SetParent(keys[0].MainWindowHandle, this.Handle);
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWnd, IntPtr hWndChild);
+
+    }
+}
